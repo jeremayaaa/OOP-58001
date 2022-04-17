49da171056095ea4456905413d59f83c5f13024c
jeremayaaa <98925416+jeremayaaa@users.noreply.github.com>
2022-04-17 18:22:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 from tkinter import *
 window=Tk()
+def change():
+    label['bg']='yellow'
 
-def fullname():
-    name['text'] = 'myname'
+button = Button(text='<---Click to change the color of the button', command=change)
+button.place(x=120, y=150)
 
-label = Label(text='Enter your Fullname:', fg='red')
-label.place(x=30, y=50)
+label=Label(text='First number', fg='red', bg='blue')
+label.place(x=22, y=153)
 
-button = Button(text='Click to display your Fullname', fg='red', command=fullname)
-button.place(x=30, y=100)
-
-entername = Entry(textvariable='myname', bd='5')
-entername.place(x=230, y=50)
-
-name = Entry(text='This is my name', bd='5')
-name.place(x=230, y=100)
-
-window.title('Midterm in OOP')
-window.geometry("500x200+10+10")
+window.title('Hello Python')
+window.geometry("400x200+10+10")
 window.mainloop()
