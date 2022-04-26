8ca839395a407cdb4e3b019ac747108889fb27ea
jeremayaaa <98925416+jeremayaaa@users.noreply.github.com>
2022-04-26 03:39:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
 from tkinter import *
-window=Tk()
-def change():
-    label['bg']='yellow'
 
-button = Button(text='<---Click to change the color of the button', command=change)
-button.place(x=120, y=150)
+window = Tk()
+window.geometry("450x300+20+10")
+window.title("The Grid Manager")
+ent1 = Entry(window, bd=3,justify="center", bg="red")
+ent1.grid(row=0,column=0)
+ent1.insert(0,"row 0,column 0")
 
-label=Label(text='First number', fg='red', bg='blue')
-label.place(x=22, y=153)
+ent2 = Entry(window,bd=3,justify="center", bg="orange")
+ent2.grid(row=0, column = 1)
+ent2.insert(0,"row 0, column 1")
 
-window.title('Hello Python')
-window.geometry("400x200+10+10")
+ent3 = Entry(window,bd=3,justify="center", bg="yellow")
+ent3.grid(row=0, column = 2)
+ent3.insert(0,"row 0, column 2")
+
+ent4 = Entry(window,bd=3,justify="center", bg="green")
+ent4.grid(row=1, column = 0)
+ent4.insert(0,"row 1, column 0")
+
+ent5 = Entry(window,bd=3,justify="center", bg="blue")
+ent5.grid(row=1, column = 1)
+ent5.insert(0,"row 1, column 1")
+
+ent6 = Entry(window,bd=3,justify="center", bg="violet")
+ent6.grid(row=1, column = 2)
+ent6.insert(0,"row 1, column 2")
+
+ent7 = Entry(window,bd=3,justify="center", bg="red")
+ent7.grid(row=2, column = 0)
+ent7.insert(0,"row 2, column 0")
+
+ent8 = Entry(window,bd=3,justify="center", bg="red")
+ent8.grid(row=2, column = 1)
+ent8.insert(0,"row 2, column 1")
+
+ent9 = Entry(window,bd=3,justify="center", bg="red")
+ent9.grid(row=2, column = 2)
+ent9.insert(0,"row 2, column 2")
+
+yscroll = Scrollbar(window,orient=VERTICAL)
+yscroll.grid(row = 5,column = 2,rowspan=4,padx=(0,100),pady=5)
+
+datalist = "Student1", "Student2", "Student3", "Student4", "Student5", "Student6","Student7", "Student8", "Student9", "Student10"
+var = StringVar()
+
+lb = Listbox(window,listvariable=var,width=10, height=4,yscrollcommand=yscroll.set)
+lb.grid(row=5,column=1, rowspan=4,padx=(100,0),pady=5)
+var.set(tuple(datalist))
+yscroll["command"] = lb.yview()
+
+
 window.mainloop()
